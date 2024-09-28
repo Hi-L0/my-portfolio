f2a236c573121c62f0434e338a08b82ccd3bb325
hilo <7amzalaaroussi@gmail.com>
2024-09-28 20:59:38 +0100

    fix the spotlight effect (keeps getting big in some random positions)

--- src/app/components/effects/SpotlightEffect.tsx
+++ src/app/components/effects/SpotlightEffect.tsx
@@ -9,10 +9,10 @@ function SpotlightEffect({position}:{position:positionType}) {
     
     return (
         <div
-        className="fixed pointer-events-none  z-10 -inset-0 h-screen opacity-100 transition duration-300"
+        className="fixed pointer-events-none  z-10 -inset-0 h-screen opacity-5 transition duration-300"
         style={{
           opacity:1,
-          background: `radial-gradient(circle at ${position.x}px ${position.y}px, rgba(74,267,235,.15),rgba(74,167,235,.1),rgba(74,167,235,.06), transparent 50%)`,
+          background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(74,267,235,.15),rgba(74,167,235,.1),rgba(74,167,235,.06), transparent 50%)`,
         }}
       />
 
