cb8f2e986345b7debff391af553fee63aebb1f40
hilo <7amzalaaroussi@gmail.com>
2024-10-22 15:25:57 +0100

    added rsuite package for timeline in the experience section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ScrollProvider } from "./contexts/scrollProvider";
-
+import 'rsuite/dist/rsuite.min.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
