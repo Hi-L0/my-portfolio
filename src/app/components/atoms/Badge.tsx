b72719e6366c3d308c511337c72dc33fa2682da4
hilo <7amzalaaroussi@gmail.com>
2024-10-22 15:24:46 +0100

    modify the badge styling and adding props to the badge component

--- src/app/components/atoms/Badge.tsx
+++ src/app/components/atoms/Badge.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Badge() {
+type badgeProps={
+  title:string;
+}
+function Badge(props:badgeProps) {
   return (
     <span className='py-3 text-center'>
-        <p className='text-slate-600 bg-[#4AFFEB] w-16 rounded'>
-          Badge
+        <p className='text-slate-600 bg-[#4AFFEB] w-16 py-1 rounded'>
+          {props.title}
         </p>
     </span>
   )
