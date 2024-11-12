77005dbfb5b92b40a4118b93f42a0bcdd590b546
hilo <7amzalaaroussi@gmail.com>
2024-11-12 16:16:38 +0100

    fix: navlinks style

--- src/app/components/atoms/Links.tsx
+++ src/app/components/atoms/Links.tsx
@@ -5,11 +5,11 @@ import '../style.css'
 function Links({link,selected_id}:{link:linkType,selected_id:number | undefined}) {
 
   return (
-    <div className='py-3 uppercase font-medium link'>
+    // <div className='py-3 uppercase font-medium link'>
         <a 
           href={`#${link.title}`} 
           key={link.id}
-          className={`flex text-sm tracking-widest ${selected_id===link.id  ? "active" : "flex text-slate-500 hover:text-slate-100"}`}
+          className={`py-3 uppercase font-medium link flex text-sm tracking-widest ${selected_id===link.id  ? "active" : "flex text-slate-500 hover:text-slate-100"}`}
         >
           <span className={selected_id===link.id  ? "active-span"
           // "bg-slate-100 mt-3 mr-3 h-[1px] w-16  duration-150 ease-in-out"
@@ -17,7 +17,7 @@ function Links({link,selected_id}:{link:linkType,selected_id:number | undefined}
             ' mt-2 mr-3 h-[2px] w-8 bg-slate-500 hover:bg-slate-100'} id='point'></span>
             {link.title}
         </a>
-    </div>
+    // </div>
   )
 }
 
