00bcb62528ee7939ba724a3660fe2264ab463a26
hilo <7amzalaaroussi@gmail.com>
2024-11-22 13:50:10 +0100

    added flex to badges

--- src/app/components/atoms/Badge.tsx
+++ src/app/components/atoms/Badge.tsx
@@ -5,8 +5,8 @@ type badgeProps={
 }
 function Badge(props:badgeProps) {
   return (
-    <span className='py-3 text-center'>
-        <p className='text-slate-600 bg-[#4AFFEB] w-16 py-1 rounded'>
+    <span className={` rounded text-center w-20 bg-[#4AFFEB] max-w-lg ${props.title.split(" ").length >1 ? "py-0" :"py-1" }`}>
+        <p className='text-slate-600  w-20  rounded'>
           {props.title}
         </p>
     </span>
--- src/app/components/molecules/ExeprienceCard.tsx
+++ src/app/components/molecules/ExeprienceCard.tsx
@@ -2,30 +2,39 @@ import React from 'react'
 import Badge from '../atoms/Badge'
 import { IoOpenOutline } from 'react-icons/io5'
 
-function ExeprienceCard({displayed}:{displayed:boolean}) {
+function ExeprienceCard({data,displayed, selected}:{data:experienceType,displayed:boolean, selected:number}) {
+   
   return (
     <div>
         <div className={`group relative my-10 grid pb-3 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50 ${displayed?'opacity-100':"opacity-40"}`}>
-            <header className='sm:col-span-2 mb-2 mt-1 text-sm '>
-                2022 - 2022
+            <header className='sm:col-span-2 mb-2 mt-2 text-xs '>
+                {data.start_date} - {data.end_date}
             </header>
             <div className='absolute -inset-x-4 -inset-y-4 -z-10 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/30 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg '></div>
             <div className='sm:col-span-6'>
                 <div className=' hover:text-3xl'>
-                    <a href='' className={`flex justify-items-start justify-between font-semibold hover:text-teal-400 hover:no-underline`}>
+                    <a href={data.post.project_url} target='_blank' className={`flex justify-items-start justify-between text-[#cbd5e1] font-semibold hover:text-teal-400 ${!displayed ?  selected===data.id? 'text-teal-400':"":""} no-underline hover:no-underline`}>
                         <h1 className='text-2xl mb-3 '>
-                            post 
+                            {data.post.title} - {data.company}
                         </h1>
-                        <span className=''> <IoOpenOutline size={25}/></span>
+                        {data.post.project_url != "#" && 
+                            <div className='text-2xl mx-1'>
+                                <IoOpenOutline size={25}/>
+                            </div> 
+                        }
                     </a>
                 </div>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque deleniti voluptates laudantium obcaecati dignissimos, est ab molestiae blanditiis quis recusandae ad, consequuntur non et deserunt dolor ea labore sed mollitia!
+                    {data.post.description}    
                 </p>
                 {/* badge section */}
-                <div className='my-2 flex justify-start gap-3'>
+                <div className='my-2 grid lg:grid-cols-5 grid-cols-4 gap-4'>
                 {/* <Badge/><Badge/><Badge/><Badge/><Badge/> */}
-                        <Badge title='test'/>
+                        { data.post.tech_used && data.post.tech_used.map(
+                            item=> <Badge title={item}/>
+                        )
+                            
+                        }
                     {/* <p className='text-[#4AFFEB]'>
                     </p> */}
                 </div>
