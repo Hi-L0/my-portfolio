a2914991043749c4a1508e2b10d0a54a461d9d0d
hilo <7amzalaaroussi@gmail.com>
2025-01-08 14:52:12 +0100

    modified: background gradient and the spotlight effect

--- src/app/components/effects/SpotlightEffect.tsx
+++ src/app/components/effects/SpotlightEffect.tsx
@@ -12,7 +12,11 @@ function SpotlightEffect({position}:{position:positionType}) {
         className="fixed pointer-events-none  z-10 -inset-0 h-screen opacity-5 transition duration-300"
         style={{
           opacity:1,
-          background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(74,267,235,.15),rgba(74,167,235,.1),rgba(74,167,235,.06), transparent 50%)`,
+          background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(74,267,235,.50),rgba(74,167,235,.20),rgba(74,167,235,.06), transparent 50%)`,
+          // radial-gradient(
+          //   farthest-corner circle at 51% 50%,
+          //   #00dfff 0%, 25%, #8c00ea 100%
+          // );
         }}
       />
 
